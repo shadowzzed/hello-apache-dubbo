7e328c0b0fa3126e69dd9e6763f3c188b5cde1bd
Zed <shadowl91@163.com>
2020-02-21 22:50:23 +0800

    create project and dependencies

---
+++ src/main/java/com/zed/hello/apache/dubbo/HelloApacheDubboApplication.java
@@ -0,0 +1,13 @@
+package com.zed.hello.apache.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApacheDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApacheDubboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zed/hello/apache/dubbo/HelloApacheDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zed.hello.apache.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApacheDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
